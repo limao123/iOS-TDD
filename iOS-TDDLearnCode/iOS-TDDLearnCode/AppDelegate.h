0b4a2d57824b397b9defff2856f4c71d69dcc6f5
limaofuyuanzhang <397524331@qq.com>
2016-04-06 18:00:35 +0800

    TDD example project

---
+++ iOS-TDDLearnCode/iOS-TDDLearnCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-TDDLearnCode
+//
+//  Created by bmob-LT on 16/4/6.
+//  Copyright © 2016年 bmob-LT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
